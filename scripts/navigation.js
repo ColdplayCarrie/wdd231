39e44b95046aa0e358f2a37a0de0c7c60a2e022b
ColdplayCarrie <yearbookstuff5@gmail.com>
2025-09-13 18:26:11 -0500

    231 Project - first save

---
+++ scripts/navigation.js
@@ -0,0 +1,15 @@
+const navButton = document.querySelector('#ham-btn');
+const navBar = document.querySelector('#nav-bar');
+
+const year = document.querySelector("#year");
+const today = new Date();
+
+year.innerHTML = `<span id="year">${today.getFullYear()}</span>`;
+
+let olastModified = document.querySelector("#lastModified");
+olastModified.innerHTML = `<span id="lastModified">${document.lastModified}</span>`;
+
+navButton.addEventListener('click', () => {
+    navButton.classList.toggle('show');
+    navBar.classList.toggle('show');
+});
