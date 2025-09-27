dbde7d9e23fd481b8ff19d546feeb50b6425b16c
ColdplayCarrie <yearbookstuff5@gmail.com>
2025-09-27 16:37:23 -0500

    week 3 stuff

--- chamber/scripts/index.js
+++ chamber/scripts/index.js
@@ -80,3 +80,77 @@ function displayForecastResults(data) {
     forecastDayTwo.innerHTML = formatForecast(dailyForecasts[1]);
     forecastDayThree.innerHTML = formatForecast(dailyForecasts[3]);
 }
+ 
+//member spotlights//
+const directoryUrl = "https://ColdplayCarrie.github.io/wdd231/chamber/data/members.json";
+const spotlightContainer = document.querySelector("#spotlight-container");
+
+async function getMemberData() {
+    const response = await fetch(directoryUrl);
+    const data = await response.json();
+    const members = data.members;
+    // console.log("fetched member data:", members);
+    displaySpotlightMembers(members);
+}
+
+getMemberData();
+
+function getSpotlightMembers(members) {
+    const eligibleMembers = members.filter(member => {
+        const level = member.membershipLevel;
+        return level === "2" || level === "3";
+    });
+
+    const desiredNumber = Math.min(3, eligibleMembers.length);
+    const randomMembers = eligibleMembers.sort(() => 0.5 - Math.random());
+    return randomMembers.slice(0, desiredNumber);
+}
+
+const displaySpotlightMembers = (allMembers) => {
+    if (!spotlightContainer) {
+        console.error("Spotlight container not found");
+        return;
+    }
+
+    const spotlights = getSpotlightMembers(allMembers);
+
+    spotlights.forEach((member) => {
+        let card = document.createElement("section");
+        card.classList.add("spotlight-card");
+
+        let memberName = document.createElement("h3");
+        let memberStreetAddress = document.createElement("p");
+        let memberCityStateCountry = document.createElement("p");
+        let memberLogo = document.createElement("img");
+        let memberPhone = document.createElement("p");
+        let memberWebsite = document.createElement("a");
+        let memberSince = document.createElement("p");
+        let memberLevel = document.createElement("p");
+
+        memberName.textContent = `${member.name}`;
+        memberStreetAddress.textContent = `${member.address.street}`
+        memberCityStateCountry.textContent = `${member.address.city}, ${member.address.state}, ${member.address.country} ${member.address.zip}`;
+        memberLogo.textContent = `${member.imageOrIconFile}`;
+        memberPhone.textContent = `${member.phoneNumber}`;
+        memberWebsite.textContent = `${member.websiteUrl}`;
+        memberSince.textContent = `Member since ${member.memberSince}`;
+        memberLevel.textContent = `Membership Level: ${member.membershipLevel}`;
+
+        memberLogo.setAttribute("src", member.imageOrIconFile);
+        memberLogo.setAttribute("alt", `Business card for ${member.name}`);
+        memberLogo.setAttribute("loading", "lazy");
+        memberLogo.setAttribute("width", "150");
+        memberLogo.setAttribute("height", "150");
+
+        card.appendChild(memberLogo);
+        card.appendChild(memberName);
+        card.appendChild(memberStreetAddress);
+        card.appendChild(memberCityStateCountry);
+        card.appendChild(memberPhone);
+        card.appendChild(memberWebsite);
+        card.appendChild(memberSince);
+        card.appendChild(memberLevel);
+
+        spotlightContainer.appendChild(card);
+    });
+}
