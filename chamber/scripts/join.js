943e8c16c79570031222a5db1060c0487a9425b9
ColdplayCarrie <yearbookstuff5@gmail.com>
2025-09-28 19:30:03 -0500

    week 4 not quite finished

--- chamber/scripts/join.js
+++ chamber/scripts/join.js
@@ -7,3 +7,34 @@ document.addEventListener("DOMContentLoaded", function () {
         timestamp.value = now.toISOString();
     }
 });
+
+
+/****MODALS****/
+const openLevel1 = document.querySelector("#open-level1");
+const openLevel2 = document.querySelector("#open-level2");
+const openLevel3 = document.querySelector("#open-level3");
+const openLevel4 = document.querySelector("#open-level4");
+const membershipLevels = document.querySelector("#membership-levels");
+const closeLevels = document.querySelector("#close-levels");
+const membershipLevelsText = document.querySelector("#membership-levels div");
+
+openLevel1.addEventListener("click", () => {
+    membershipLevels.showModal();
+    membershipLevelsText.innerHTML = `<strong>NP Membership (for non-profit organizations only) - Free ($0)</strong> - Includes advertising benefits, 5 weekly service hours to aid your organization as well as free entry to all Chamber of Commerce events`;
+})
+openLevel2.addEventListener("click", () => {
+    membershipLevels.showModal();
+    membershipLevelsText.innerHTML = `<strong>Bronze Membership - $5.00 p/month</strong> - Includes advertising benefits, placard for display in business/organization, as well as free entry to all Chamber of Commerce events`;
+})
+openLevel3.addEventListener("click", () => {
+    membershipLevels.showModal();
+    membershipLevelsText.innerHTML = `<strong>Silver Membership - $9.00 p/month</strong> - Includes advertising benefits, placard for display in business/organization, spotlight on the Chamber of Commerce website main page, free advertising in the quarterly magazine as well as free entry to all Chamber of Commerce events`;
+})
+openLevel4.addEventListener("click", () => {
+    membershipLevels.showModal();
+    membershipLevelsText.innerHTML = `<strong>Gold Membership - $13.00 p/month</strong> - Includes advertising benefits, placard for display in business/organization, spotlight on the Chamber of Commerce website main page, free advertising in the quarterly magazine, quarterly luncheon for all Gold Members, prominent booth at the Summer Festival as well as free entry to all Chamber of Commerce events`;
+})
+
+closeLevels.addEventListener("click", () => {
+    membershipLevels.close();
+})
