28cc7af3791c8bf0945fe59f6694bed2ba8cc77c
ColdplayCarrie <yearbookstuff5@gmail.com>
2025-10-04 22:22:22 -0500

    week 5 assignment 5

--- chamber/scripts/discover.js
+++ chamber/scripts/discover.js
@@ -14,21 +14,27 @@ function displayPlaces(places) {
     places.forEach((place) => {
         let card = document.createElement("section");
         let placeName = document.createElement("h2");
-        let placeAddress = document.createElement("p");
+        let placeAddress = document.createElement("address");
         let placeDescription = document.createElement("p");
+        let figure = document.createElement("figure");
         let placePhoto = document.createElement("img");
-
-        placeName.textContent = `${place.name}`;
-        placeAddress.textContent = `${place.address}`
-        placeDescription.textContent = `${place.description}`;
+        // let learnMoreBtn = document.createElement("button");
 
         placePhoto.setAttribute("src", place.placePhoto);
         placePhoto.setAttribute("alt", `Photo of ${place.name}`);
         placePhoto.setAttribute("loading", "lazy");
         placePhoto.setAttribute("width", "200");
         placePhoto.setAttribute("height", "300");
+        // learnMoreBtn.setAttribute("type", "button");
 
-        card.appendChild(placePhoto);
+        figure.appendChild(placePhoto);
+
+        placeName.textContent = `${place.name}`;
+        placeAddress.textContent = `${place.address}`
+        placeDescription.textContent = `${place.description}`;
+        // learnMoreBtn.textContent = "Learn More";
+
+        card.appendChild(figure);
         card.appendChild(placeName);
         card.appendChild(placeAddress);
         card.appendChild(placeDescription);
